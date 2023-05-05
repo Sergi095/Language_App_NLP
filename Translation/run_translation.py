d8100c783bb49682c7bb1e5602d67924e856b8a2
Sergi095 <sergio.gutierrezmaury@gmail.com>
2023-05-05 21:03:51 +0200

    renaming functions

--- Translation/run_translation.py
+++ Translation/run_translation.py
@@ -14,12 +14,12 @@ from helpers import custom_standardization
 
 
 
-def main(model_path: str = '../Neural_Network/saved_model/model.h5', t: int = 1) -> None:
+def get_text_mic(t: int = 1) -> str:
 
-    print('starting main')
-    print('loading model at {model_path}'.format(model_path=model_path))
+    # print('starting main')
+    # print('loading model at {model_path}'.format(model_path=model_path))
 
-    model = load_model(model_path)
+    # model = load_model(model_path)
 
     # print(model.summary())
     # create recognizer and mic instances
@@ -30,22 +30,33 @@ def main(model_path: str = '../Neural_Network/saved_model/model.h5', t: int = 1)
 
     time = 0
     while time < t:
-        print('you have {t} times to speak'.format(t=t-time))
-        print('you can speak now')
+        # print('you have {t} times to speak'.format(t=t-time))
+        # print('you can speak now')
         speech = get_text_from_audio(recognizer, microphone)
-        print(f'you said: {speech}')
+        # print(f'you said: {speech}')
         text_to_translate += speech
         time += 1
         break
     
-    print(f'this is the text to translate: {text_to_translate}')
+    # print(f'this is the text to translate: {text_to_translate}')
+
+    # max_decoded_sentence_length = 20
+    # translation = decode_sequence(text_to_translate, max_decoded_sentence_length, model)
+    # print(f'this is the translation: {translation}')
+
+    return text_to_translate
+
+
+def do_translation(input_sentence: str, model_path: str = '../Neural_Network/saved_model/model.h5') -> str:
+
+    model = load_model(model_path)
 
     max_decoded_sentence_length = 20
-    translation = decode_sequence(text_to_translate, max_decoded_sentence_length, model)
-    # print(f'this is the translation: {translation}')
+    translation = decode_sequence(input_sentence, max_decoded_sentence_length, model)
 
     return translation
 
-
 if __name__ == "__main__":
-    main()
+    # main()
+
+    print('running')
--- Translation/utils.py
+++ Translation/utils.py
@@ -6,6 +6,7 @@ from get_text import recognize_speech_from_mic
 import speech_recognition as sr
 import tensorflow as tf
 from tensorflow import keras
+from langdetect import detect
 
 import pickle
 from keras.layers import Input
@@ -74,19 +75,42 @@ def decode_sequence(input_sentence: str,
     spa_vocab = spa_vectorization.get_vocabulary()
     spa_index_lookup = dict(zip(range(len(spa_vocab)), spa_vocab))
 
-    tokenized_input_sentence = eng_vectorization([input_sentence])
-    decoded_sentence = "[start]"
-    for i in range(max_decoded_sentence_length):
-        tokenized_target_sentence = spa_vectorization([decoded_sentence])[:, :-1]
-        predictions = transformer([tokenized_input_sentence, tokenized_target_sentence])
+    eng_vocab = eng_vectorization.get_vocabulary()
+    eng_index_lookup = dict(zip(range(len(eng_vocab)), eng_vocab))
 
-        sampled_token_index = np.argmax(predictions[0, i, :])
-        sampled_token = spa_index_lookup[sampled_token_index]
-        decoded_sentence += " " + sampled_token
+    lang_detect = detect(input_sentence)
+    print(lang_detect)
+    # if sentence is in english, translate to spanish
+    if lang_detect == 'en':
+        tokenized_input_sentence = eng_vectorization([input_sentence])
+        decoded_sentence = "[start]"
+        for i in range(max_decoded_sentence_length):
+            tokenized_target_sentence = spa_vectorization([decoded_sentence])[:, :-1]
+            predictions = transformer([tokenized_input_sentence, tokenized_target_sentence])
 
-        if sampled_token == "[end]":
-            break
-    return decoded_sentence
+            sampled_token_index = np.argmax(predictions[0, i, :])
+            sampled_token = spa_index_lookup[sampled_token_index]
+            decoded_sentence += " " + sampled_token
+
+            if sampled_token == "[end]":
+                break
+        return decoded_sentence
+    elif lang_detect == 'es':
+        tokenized_input_sentence = spa_vectorization([input_sentence])
+        decoded_sentence = "[start]"
+        for i in range(max_decoded_sentence_length):
+            tokenized_target_sentence = eng_vectorization([decoded_sentence])[:, :-1]
+            predictions = transformer([tokenized_input_sentence, tokenized_target_sentence])
+
+            sampled_token_index = np.argmax(predictions[0, i, :])
+            sampled_token = eng_index_lookup[sampled_token_index]
+            decoded_sentence += " " + sampled_token
+
+            if sampled_token == "[end]":
+                break
+        return decoded_sentence
+    else:
+        raise Exception('Language not supported')
 
 
 
