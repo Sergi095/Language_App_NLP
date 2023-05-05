2de5982872829cdcf3134035eda26d75143dfdec
Sergi095 <sergio.gutierrezmaury@gmail.com>
2023-05-05 21:04:17 +0200

    first version running, the model is not that good

--- app/front_end/static/script.js
+++ app/front_end/static/script.js
@@ -18,3 +18,79 @@ outputLanguageSelector.addEventListener('change', function() {
         inputLanguageSelector.value = 'English';
     }
 });
+
+document.addEventListener("DOMContentLoaded", function() {
+    var translateButton = document.getElementById("translate-button");
+    var recordButton = document.getElementById("record-button");
+    var inputText = document.getElementById("input_text");
+    var outputText = document.getElementById("output_text");
+    var inputLanguageSelector = document.getElementById("input-language-selector");
+    var outputLanguageSelector = document.getElementById("output-language-selector");
+  
+    translateButton.addEventListener("click", function() {
+      var inputSentence = inputText.value;
+      var inputLanguage = inputLanguageSelector.value;
+      var outputLanguage = outputLanguageSelector.value;
+      var xhr = new XMLHttpRequest();
+      xhr.open("POST", "/translate", true);
+      xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+      xhr.onreadystatechange = function() {
+        if (xhr.readyState === 4 && xhr.status === 200) {
+          var response = JSON.parse(xhr.responseText);
+          outputText.value = response.translation;
+        }
+      };
+      xhr.send("input_text=" + inputSentence + "&input_language_selector=" + inputLanguage + "&output_language_selector=" + outputLanguage);
+    });
+  
+    recordButton.addEventListener("click", function() {
+      var inputLanguage = inputLanguageSelector.value;
+      var outputLanguage = outputLanguageSelector.value;
+      var xhr = new XMLHttpRequest();
+      xhr.open("POST", "/translate", true);
+      xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+      xhr.onreadystatechange = function() {
+        if (xhr.readyState === 4 && xhr.status === 200) {
+          var response = JSON.parse(xhr.responseText);
+          outputText.value = response.translation;
+          inputText.value = response.input_text;
+        }
+      };
+      xhr.send("input_language_selector=" + inputLanguage + "&output_language_selector=" + outputLanguage + "&record=true");
+    });
+  });
+
+  function translate() {
+    // get the input text from the input-text box
+    var inputText = document.getElementById("input_text").value;
+    // get the selected language options
+    var inputLang = document.getElementById("input-language-selector").value;
+    var outputLang = document.getElementById("output-language-selector").value;
+
+    // if recording is enabled, add the recorded text to the input-text box
+    if (document.getElementById('record-toggle').checked) {
+        inputText = document.getElementById('recorded-text').value;
+    }
+
+    // make a POST request to the /translate endpoint with the input text and selected languages
+    fetch('/translate', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded'
+      },
+      body: new URLSearchParams({
+        'input_text': inputText,
+        'input_language_selector': inputLang,
+        'output_language_selector': outputLang
+      })
+    })
+    .then(response => response.json())
+    .then(data => {
+      // set the output text in the output-text box
+      document.getElementById("output_text").value = data.output_text;
+      // set the input text in the input-text box (for transcription)
+      document.getElementById("input_text").value = data.input_text;
+    });
+  
+  }
+  
--- app/translator.py
+++ app/translator.py
@@ -1,9 +1,12 @@
 import sys
 import os
 sys.path.append('../Translation')
-from run_translation import main
-from flask import Flask, render_template
+from run_translation import *
+from flask import Flask, render_template, request
+import json
 
+sys.path.append('../Neural_Network')
+from helpers import custom_standardization
 
 app = Flask(__name__, 
             template_folder='front_end',
@@ -12,16 +15,39 @@ app = Flask(__name__,
 
 
 @app.route('/')
-def index():
+def home():
     # return render_template(os.path.abspath("front_end/index.html"))
     return render_template("index.html")    
 
-# @app.route("/")
-# def home():
-#     return 'Flask server is running HEHE'
-
-
-
-
+@app.route('/translate', methods=['POST'])
+def translate():
+    input_sentence = ''
+    if 'record' in request.form:
+        input_sentence = get_text_mic()        
+        input_language = request.form['input_language_selector']
+        output_language = request.form['output_language_selector']
+        translation = do_translation(input_sentence)
+        # set the transcribed text in the input text box
+        response = {
+            'input_text': input_sentence,
+            'recorded-text': input_sentence,
+            'output_text': translation,
+            'input_language_selector': input_language,
+            'output_language_selector': output_language,
+            'translation': translation
+        }
+    else:
+        input_sentence = request.form['input_text']
+        input_language = request.form['input_language_selector']
+        output_language = request.form['output_language_selector']
+        translation = do_translation(input_sentence)
+        response = {
+            'input_text': input_sentence,
+            'output_text': translation,
+            'input_language_selector': input_language,
+            'output_language_selector': output_language,
+            'translation': translation
+        }
+    return json.dumps(response)
 if __name__ == "__main__":
     app.run(debug=True)
