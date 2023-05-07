23a38bf0c7a37b1414fa47c7d5b8c5f9863f06bb
Sergi095 <sergio.gutierrezmaury@gmail.com>
2023-05-07 11:58:34 +0200

    adaptin new model

--- app/translator.py
+++ app/translator.py
@@ -19,35 +19,70 @@ def home():
     # return render_template(os.path.abspath("front_end/index.html"))
     return render_template("index.html")    
 
+# @app.route('/translate', methods=['POST'])
+# def translate():
+#     input_sentence = ''
+#     if 'record' in request.form:
+#         input_sentence = get_text_mic()        
+#         input_language = request.form['input_language_selector']
+#         output_language = request.form['output_language_selector']
+#         translation = do_translation(input_sentence)
+#         # set the transcribed text in the input text box
+#         response = {
+#             'input_text': input_sentence,
+#             'recorded-text': input_sentence,
+#             'output_text': translation,
+#             'input_language_selector': input_language,
+#             'output_language_selector': output_language,
+#             'translation': translation
+#         }
+#     else:
+#         input_sentence = request.form['input_text']
+#         input_language = request.form['input_language_selector']
+#         output_language = request.form['output_language_selector']
+#         translation = do_translation(input_sentence)
+#         response = {
+#             'input_text': input_sentence,
+#             'output_text': translation,
+#             'input_language_selector': input_language,
+#             'output_language_selector': output_language,
+#             'translation': translation
+#         }
+#     return json.dumps(response)
 @app.route('/translate', methods=['POST'])
 def translate():
-    input_sentence = ''
     if 'record' in request.form:
-        input_sentence = get_text_mic()        
-        input_language = request.form['input_language_selector']
-        output_language = request.form['output_language_selector']
-        translation = do_translation(input_sentence)
-        # set the transcribed text in the input text box
-        response = {
-            'input_text': input_sentence,
-            'recorded-text': input_sentence,
-            'output_text': translation,
-            'input_language_selector': input_language,
-            'output_language_selector': output_language,
-            'translation': translation
-        }
+        # get input sentence from microphone
+        input_sentence = get_text_mic() 
     else:
+        # get input sentence from text input field
         input_sentence = request.form['input_text']
-        input_language = request.form['input_language_selector']
-        output_language = request.form['output_language_selector']
-        translation = do_translation(input_sentence)
-        response = {
-            'input_text': input_sentence,
-            'output_text': translation,
-            'input_language_selector': input_language,
-            'output_language_selector': output_language,
-            'translation': translation
-        }
+        
+    input_language = request.form['input_language_selector']
+    output_language = request.form['output_language_selector']
+    
+    if input_language == 'english' and output_language == 'spanish':
+        # translate english to spanish
+        translation = do_translation2(input_sentence, input_language='english')
+    elif input_language == 'spanish' and output_language == 'english':
+        # translate spanish to english
+        translation = do_translation2(input_sentence, input_language='spanish')
+    else:
+        # unsupported language pair
+        translation = 'Unsupported language pair'
+        
+    response = {
+        'input_text': input_sentence,
+        'output_text': translation,
+        'input_language_selector': input_language,
+        'output_language_selector': output_language,
+        'translation': translation
+    }
+    
     return json.dumps(response)
+
+
+
+
 if __name__ == "__main__":
     app.run(debug=True)
